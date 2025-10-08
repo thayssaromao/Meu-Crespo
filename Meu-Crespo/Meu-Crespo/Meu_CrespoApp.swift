e5f627faebc8eba78d21c52c87ea398bcc26e533
thayssaromao <romao@alunos.utfpr.edu.br>
2025-10-08 18:37:44 -0300

    Initial commit

---
+++ Meu-Crespo/Meu-Crespo/Meu_CrespoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meu_CrespoApp.swift
+//  Meu-Crespo
+//
+//  Created by Thayssa Romão on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Meu_CrespoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meu-Crespo/Meu-Crespo/Views/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Meu-Crespo
+//
+//  Created by Thayssa Romão on 08/10/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
