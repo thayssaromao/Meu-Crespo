0348192c357258e766bf539c87a307886f563226
thayssaromao <romao@alunos.utfpr.edu.br>
2025-10-08 23:01:36 -0300

    Add LearnView SwiftUI view
    Introduces a new LearnView SwiftUI component displaying a placeholder text. This sets up the structure for the learning screen in the Meu-Crespo app.

---
+++ Meu-Crespo/Meu-Crespo/Views/LearnView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnView.swift
+//  Meu-Crespo
+//
+//  Created by Thayssa Romão on 08/10/25.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text("Tela de Aprendizado Crespo!")
+    }
+}
+
+#Preview {
+    LearnView()
+}
